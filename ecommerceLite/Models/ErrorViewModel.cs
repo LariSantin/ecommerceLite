1245c3f1ececea577d97ef6d99cda75a709aa81a
Datacoper <datacoper@ac-cas.unipar.br>
2019-01-25 14:59:10 -0200

    Adicionar arquivos de projeto.

---
+++ ecommerceLite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ecommerceLite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
